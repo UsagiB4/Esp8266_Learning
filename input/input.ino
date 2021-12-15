e2317cb14c475b3e5f4ecd9af02e29cc30679e04
UsagiB4 <binaryBunny@tutanota.com>
2021-12-15 15:54:11 +0600

    Digital input

--- input/input.ino
+++ input/input.ino
@@ -1,21 +1,22 @@
-const int ledPin = 4;
-const int btnPin = 5;
+// seting input and output pins
+const int ledPin = 4; // led pin as D2 == GPIO-1
+const int btnPin = 5; // button pin as D1 == GPIO-2
 
-int btnState = 0;
+int btnState = 0;  // initial state of Button pin set to 0
 
 void setup(){
-  pinMode(btnPin, INPUT);
-  pinMode(ledPin, OUTPUT);
+  pinMode(btnPin, INPUT); // initializing button pin as Input
+  pinMode(ledPin, OUTPUT);  // initializing LED pin as Output
 
 }
 
 void loop(){
-  btnState = digitalRead(btnPin);
+  btnState = digitalRead(btnPin);  // This will read the value of Button pin
 
   if(btnState == HIGH){
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin, HIGH);  // if button is pressed(high), LED is on(high)
   } else{
-    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin, LOW); // if button is not pressed(low), LED is off(low)
   }
   
 }
