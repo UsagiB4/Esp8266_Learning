9d132f32260e05e4b6d55e76bdac6709679f891f
UsagiB4 <binaryBunny@tutanota.com>
2021-12-16 00:58:31 +0600

    Analog input and serial monitor

--- analogInput/analogInput.ino
+++ analogInput/analogInput.ino
@@ -1,4 +1,4 @@
-const int analogPin = A0;
+const int analogPin = A0;  // esp has only one analog input mode, which is A0 on board.
 int reader = 0;
 void setup(){
   pinMode(analogPin, INPUT);
@@ -6,7 +6,6 @@ void setup(){
 }
 
 void loop(){
-  analogWriteRange(255);
   reader = analogRead(analogPin);
   Serial.print("Sensor Value:");
   Serial.print(reader);
